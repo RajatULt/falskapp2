ee1847b5ffe98528ac20e5a5a7e277c3e1a7ff73
RajatULt <rajat.bhati04@gmail.com>
2024-06-28 15:47:03 +0530

    tried to update the code4

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import os
-from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    print('Request for index page received')
-    return "Hello, Azure Web Apps!"
+   print('Request for index page received')
+   return render_template('index.html')
 
 @app.route('/favicon.ico')
 def favicon():
@@ -15,14 +18,15 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-    name = request.form.get('name')
+   name = request.form.get('name')
 
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return render_template('hello.html', name=name)
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return redirect(url_for('index'))
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    app.run()
+   app.run()
